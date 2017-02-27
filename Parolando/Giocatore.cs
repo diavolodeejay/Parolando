bbf4c6a06907661369157e0d99a14ca4942c99a9
Gabriele <gabriele.dinuovo@gmail.com>
2017-02-27 11:49:06 +0100

    Starting to write some code
    No sense code obv

---
+++ Parolando/Giocatore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parolando
+{
+    class Giocatore
+    {
+        private string mNome;
+        public Sacchetto sacca;
+
+
+        public string Nome
+        {
+            get
+            {
+                return mNome;
+            }
+            set
+            {
+                mNome = value;
+            }
+        }
+
+
+        public Giocatore(string nome)
+        {
+            this.mNome = nome;
+            this.sacca = new Sacchetto();
+        }
+    }
+}
---
+++ Parolando/Gioco.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parolando
+{
+    class Gioco
+    {
+        Giocatore[] players;
+        public void Gioca(int numeroPlayers)
+        {
+            players = new Giocatore[numeroPlayers];
+            for(int count = 0; count<players.Length;count++)
+            {
+                players[count] = new Giocatore(count.ToString());
+            }
+
+        }
+    }
+}
